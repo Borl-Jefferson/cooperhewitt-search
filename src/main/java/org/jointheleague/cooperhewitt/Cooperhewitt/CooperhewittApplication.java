a576a57a95a9e0b09197ffc9bd42a8c796ff4862
Aarit Jayaswal <aaritrj@gmail.com>
2026-03-02 16:47:20 -0800

    initial

---
+++ src/main/java/org/jointheleague/cooperhewitt/Cooperhewitt/CooperhewittApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.cooperhewitt.Cooperhewitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperhewittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperhewittApplication.class, args);
+	}
+
+}

562597a1eb19f41eecfa964ebd90c181456e666d
Ruizosvaldo <oruiz.code@gmail.com>
2026-03-11 16:37:21 -0700

    patched the Spring Boot issue

--- src/main/java/org/jointheleague/cooperhewitt/Cooperhewitt/config/ApiDocConfig.java
+++ src/main/java/org/jointheleague/cooperhewitt/Cooperhewitt/config/ApiDocConfig.java
@@ -1,39 +1,23 @@
 package org.jointheleague.cooperhewitt.Cooperhewitt.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
 public class ApiDocConfig {
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Level 7 Cooper Hewitt search",
-                "League of Amazing Programmers Level 7 Cooper Hewitt search",
-                "1.0.0",
-                null,
-                new Contact("Matt Freedman", "www.jointheleague.org", "matt.freedman@jointheleague.org"),
-                null, null, Collections.emptyList());
-    }
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cooper Hewitt API")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Matt Freedman")
+                                .url("www.jointheleague.org")
+                                .email("matt.freedman@jointheleague.org")));
     }
 }

4416978fcf4e9119655ea0f9abcda30a59e06302
Aarit Jayaswal <aaritrj@gmail.com>
2026-04-06 17:32:36 -0700

    create pojo wrapper

--- src/main/java/org/jointheleague/cooperhewitt/Cooperhewitt/Repository/CHRepository.java
+++ src/main/java/org/jointheleague/cooperhewitt/Cooperhewitt/Repository/CHRepository.java
@@ -1,32 +1,41 @@
 package org.jointheleague.cooperhewitt.Cooperhewitt.Repository;
 
+import org.springframework.graphql.client.HttpGraphQlClient;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Repository
 public class CHRepository {
 
-    private final WebClient webClient;
-    private static final String baseUrl = "https://api.cooperhewitt.org/?query={object(title:spoon){description}}";
+    //private final WebClient webClient;
+    //private static final String baseUrl = "https://api.cooperhewitt.org/?query={object(title:\"spoon\"){title,description}}";
+    private static final String baseUrl = "https://api.cooperhewitt.org";
+    WebClient webClient = WebClient.create(baseUrl);
+    HttpGraphQlClient graphQlClient = HttpGraphQlClient.create(webClient);
 
     public CHRepository(){
-        webClient = WebClient
+        /*webClient = WebClient
                 .builder()
                 .baseUrl(baseUrl)
-                .build();
+                .build();*/
     }
 
     public String getResults(String q){
-        System.out.println(webClient.get()
-                /* .uri(uriBuilder -> uriBuilder
-                         .queryParam("title", q)
-                         .queryParam("at", "results")
-                         .queryParam("q", q)
-                         .build()
-                 )*/
-                .retrieve()
-                .bodyToMono(String.class)
-                .block());
+
+        HttpGraphQlClient graphQlClient = HttpGraphQlClient
+                .builder(webClient)
+                .build();
+
+// Perform requests with graphQlClient...
+
+
+        System.out.println(
+                graphQlClient.document("{object(description:\""+q+"\"){title,description}}")
+                        .retrieve("object")
+                        .toEntity(Object.class)
+                        .block()
+        );
 
         return "fin";
     }
